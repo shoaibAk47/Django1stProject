c6321b8f976db0cc1751500cfec0fc50c8507b0e
ShoaibAk47 <shoaib1999ak@gmail.com>
2020-04-26 18:41:43 +0530

    re-migration

--- travello/models.py
+++ travello/models.py
@@ -6,5 +6,5 @@ class Destination(models.Model):
     name=models.CharField(max_length=100)
     img=models.ImageField(upload_to='pics')
     desc=models.TextField()
-    price=models.IntegerField
+    price=models.IntegerField()
     offers=models.BooleanField(default=False)
