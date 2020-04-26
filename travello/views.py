5aaf151bdfaf47f6a4e9152eb932886276a3bc49
ShoaibAk47 <shoaib1999ak@gmail.com>
2020-04-26 15:05:33 +0530

    dynamic website

--- travello/views.py
+++ travello/views.py
@@ -7,18 +7,23 @@ def index(request):
     dest1.desc="I will go here some day"
     dest1.price="300"
     dest1.img="destination_1.jpg"
+    dest1.offers=False
 
     dest2=Destination()
     dest2.name="bali"
     dest2.desc="I will go here some other day"
     dest2.price="890"
     dest2.img="destination_8.jpg"
+    dest2.offers=False
+
 
     dest3=Destination()
     dest3.name="las vegas"
     dest3.desc="I will go not here some day"
     dest3.price="370"
     dest3.img="destination_9.jpg"
+    dest3.offers=True
+
 
 
     dests=[dest1,dest2,dest3]
