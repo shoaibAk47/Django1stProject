96c27ae88ef1f3da2edc2a047833e94b3a6972c3
ShoaibAk47 <shoaib1999ak@gmail.com>
2020-04-26 18:33:24 +0530

    connecting the database and creating 1st table

--- travello/models.py
+++ travello/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Destination:
-    id : int
-    name : str
-    img : str
-    desc: str
-    price: int
-    offers: bool
+class Destination(models.Model):
+    #database will automatically create id for each record
+    name=models.CharField(max_length=100)
+    img=models.ImageField(upload_to='pics')
+    desc=models.TextField()
+    price=models.IntegerField
+    offers=models.BooleanField(default=False)
